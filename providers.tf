bda47f68e2006ebefd070ee5bbfaf1a40b4fce45
Vitor Pellegrino <pellegrino@linux.com>
2023-09-10 12:01:34 +0200

    tf backend # Title: Summary, imperative, start upper case, don't end with a period

--- providers.tf
+++ providers.tf
@@ -1,8 +1,25 @@
 terraform {
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "pellegrino/ampernetacle"
+  }
+
+
   required_providers {
     oci = {
-      source  = "hashicorp/oci"
-      version = "~> 4"
+      source  = "oracle/oci"
+      version = "4.114.0"
     }
   }
+}
+
+
+provider "oci" {
+  auth                = "SecurityToken"
+  config_file_profile = "default-miguel"
+  region              = "eu-amsterdam-1"
 }
